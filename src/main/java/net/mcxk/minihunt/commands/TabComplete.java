ef782c2e3b84710035ad4a07d698262225271417
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2022-10-23 19:07:18 +0800

    优化tab补全

--- src/main/java/net/mcxk/minihunt/commands/TabComplete.java
+++ src/main/java/net/mcxk/minihunt/commands/TabComplete.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,15 +18,38 @@ import java.util.List;
  */
 public class TabComplete implements TabCompleter {
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String @NotNull [] args) {
         final String[] miniHunt = new String[]{"about", "hunter", "runner", "rstcd", "players", "start", "want", "help"};
         final String[] want = new String[]{"hunter", "runner", "waiting"};
+        final String[] adminCommands = new String[]{"hunter", "runner", "rstcd", "start"};
+        final String permission = "minihunt.admin";
 
-        if (args.length <= 1) {
-            return Arrays.asList(miniHunt);
+        if (args.length == 0) {
+            List<String> res = Arrays.asList(miniHunt);
+            if (!sender.hasPermission(permission)) {
+                for (String com : adminCommands) {
+                    res.remove(com);
+                }
+            }
+            return res;
         }
 
-        if (args.length < 3 && ConstantCommand.WANT.equalsIgnoreCase(args[0])) {
+        if (args.length == 1) {
+            List<String> res = new ArrayList<>();
+            for (String com : miniHunt) {
+                if (com.contains(args[0])) {
+                    res.add(com);
+                }
+            }
+            if (!sender.hasPermission(permission)) {
+                for (String com : adminCommands) {
+                    res.remove(com);
+                }
+            }
+            return res;
+        }
+
+        if (args.length == 2 && ConstantCommand.WANT.equalsIgnoreCase(args[0])) {
             return Arrays.asList(want);
         }
         return Collections.emptyList();
